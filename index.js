36076d5d08734c889045586574d5163b8a888de7
samuelomotayo <samuelomotay@gmail.com>
2018-10-25 06:42:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name){
+  drivers.push(name);
+  return drivers;
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name);
+  return drivers;
+}
+  
+function destructivelyRemoveLastDriver(){
+  drivers.pop();
+}
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
+}
+
+function appendDriver(name){
+  const newD =[...drivers,name];
+  return newD;
+}
+
+function prependDriver(name){
+  const newD= [name,...drivers];
+}
+
+function removeLastDriver(){
+  const newD= drivers.slice(0,drivers.length -1 )
+}
+
+function removeFirstDriver(){
+  
+}
+
